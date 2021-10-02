24b018f844c4fe1dae076c17b6b596c0f0e4eb55
Reda <ennakouri@ennakouri-XPS-13-9350>
2021-10-02 16:27:21 +0100

    clean project

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,25 +6,15 @@
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
-                </div>
-            </div>
-        </div>
     </app-layout>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
-    import Welcome from '@/Jetstream/Welcome.vue'
 
-    export default defineComponent({
+     export default {
         components: {
-            AppLayout,
-            Welcome,
-        },
-    })
+            AppLayout
+        }
+    }
 </script>
