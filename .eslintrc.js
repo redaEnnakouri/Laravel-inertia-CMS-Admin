c50bbb753ae87b2922cd7cbf9834b94adc321de3
Reda <ennakouri@ennakouri-XPS-13-9350>
2021-10-03 16:19:39 +0100

    add new roles for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    jquery: true
+
   },
   extends: [
     'plugin:vue/vue3-recommended',
@@ -17,10 +19,51 @@ module.exports = {
   rules: {
     'vue/require-default-prop': 'off',
     'vue/no-v-html': 'off',
-    'vue/require-prop-types': 'off'
+    'vue/require-prop-types': 'off',
+    'capitalized-comments': 'off',
+    'comma-dangle': [
+      'error',
+      'never'
+    ],
+    indent: [
+      'error',
+      2,
+      {
+        MemberExpression: 'off',
+        SwitchCase: 1
+      }
+    ],
+    'multiline-ternary': [
+      'error',
+      'always-multiline'
+    ],
+    'no-var': 'off',
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'object-shorthand': 'off',
+    'prefer-arrow-callback': 'off',
+    semi: [
+      'error',
+      'never'
+    ],
+    strict: 'error',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-for-loop': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/numeric-separators-style': 'off',
+    'unicorn/prefer-dataset': 'off',
+    'unicorn/prefer-includes': 'off',
+    'unicorn/prefer-module': 'off',
+    'unicorn/prefer-node-append': 'off',
+    'unicorn/prefer-query-selector': 'off',
+    'unicorn/prefer-spread': 'off',
+    'unicorn/prevent-abbreviations': 'off'
+
   },
   globals: {
-   
+
     route: false,
     axios: false,
     Vue: false,
